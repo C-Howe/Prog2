f42de012bffa8f19d197fb83ef3d8a8dabb00aaf
Alexander Schnackenberg <alexanderschnackenberg@gmail.com>
2014-07-08 12:06:23 -0700

    SystemProgramming2-1

---
+++ constants.h
@@ -0,0 +1,14 @@
+/*
+Alexander Schnackenberg
+aschnackenberg@albany.edu
+*/
+
+#define ARG_COUNT_1 4
+#define ARG_COUNT_2 3
+#define FLAG 1
+#define INFILE 2
+#define OUTFILE 3
+#define FLAGSIZE 2
+
+#define MAX_LENGTH_OF_STRING 255
+#define MAX_UNSIGNED_INTEGER 4294967295
---
+++ prototypes.h
@@ -0,0 +1,18 @@
+/*
+Alexander Schnackenberg
+aschnackenberg@albany.edu
+*/
+
+//the following are found in part2.c:
+void binary_to_text(FILE *input_file, FILE *output_file);
+void text_to_binary(FILE *input_file, FILE *output_file);
+
+/*
+The following are located in part2.c:
+Print the following statistics about the binary file to stdout:
+(a) The length of a shortest string in the input file.
+(b) The length of a longest string in the input file.
+(c) The value of the maximum integer in the input file.
+(d) The value of the minimum integer in the input file.
+*/
+void statistics(FILE *input_file);
